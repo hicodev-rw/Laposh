cd3428ad5a0f5f6eb49538f61a2b51f189415219
HIRWA Jean Claude <88590782+hicode-byte@users.noreply.github.com>
2022-12-11 12:27:28 +0200

    update

--- db/laposh_hotel.sql
+++ db/laposh_hotel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 29, 2022 at 10:11 AM
+-- Generation Time: Dec 11, 2022 at 11:26 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -45,12 +45,17 @@ CREATE TABLE `bookings` (
 --
 
 INSERT INTO `bookings` (`id`, `user_id`, `room_id`, `check_in_date`, `check_out_date`, `special_info`, `reference`, `status_id`, `created_at`, `updated_at`) VALUES
-(1, 5, 5, '2022-11-30 09:00:00', '2022-12-28 09:00:00', NULL, 'DSB-9292331', 4, '2022-11-29 05:01:16', '2022-11-29 06:13:45'),
+(1, 4, 5, '2022-11-30 09:00:00', '2022-12-28 09:00:00', NULL, 'DSB-9292331', 4, '2022-11-29 05:01:16', '2022-11-29 06:13:45'),
 (2, 5, 3, '2022-11-30 09:00:00', '2022-12-14 09:00:00', NULL, 'CRR-2453836', 1, '2022-11-29 05:06:04', '2022-11-29 05:55:36'),
 (3, 5, 4, '2022-05-10 10:02:00', '2022-06-28 10:02:00', NULL, 'HSH-1544272', 1, '2022-11-29 06:02:45', '2022-11-29 06:03:29'),
 (4, 6, 3, '2022-06-15 10:05:00', '2022-06-30 10:05:00', NULL, 'EFT-1141087', 1, '2022-11-29 06:05:36', '2022-11-29 06:06:26'),
 (5, 6, 4, '2022-07-14 10:05:00', '2022-07-29 10:05:00', NULL, 'SFM-1462327', 1, '2022-11-29 06:07:18', '2022-11-29 06:07:53'),
-(6, 6, 4, '2022-08-10 10:05:00', '2022-08-31 10:05:00', NULL, 'KHK-9766380', 1, '2022-11-29 06:09:11', '2022-11-29 06:09:45');
+(6, 6, 4, '2022-08-10 10:05:00', '2022-08-31 10:05:00', NULL, 'KHK-9766380', 1, '2022-11-29 06:09:11', '2022-11-29 06:09:45'),
+(7, 5, 2, '2022-12-09 21:29:00', '2022-12-24 21:29:00', NULL, 'JQR-5108640', 5, '2022-12-02 17:31:49', '2022-12-02 17:31:49'),
+(8, 5, 1, '2022-12-04 21:51:00', '2022-12-29 21:51:00', NULL, 'ZIG-9191227', 5, '2022-12-03 18:38:49', '2022-12-03 18:38:49'),
+(9, 7, 5, '2021-11-30 09:00:00', '2021-12-28 09:00:00', NULL, 'DSB-9292331', 4, '2022-11-29 05:01:16', '2022-11-29 06:13:45'),
+(10, 5, 4, '2022-12-05 18:49:00', '2022-12-09 22:55:00', 'hot water', 'YJX-8943563', 2, '2022-12-05 14:51:21', '2022-12-05 15:06:58'),
+(11, 5, 4, '2022-12-30 19:58:00', '2023-01-01 19:58:00', NULL, 'GHO-9339651', 1, '2022-12-05 16:06:09', '2022-12-05 16:12:23');
 
 -- --------------------------------------------------------
 
@@ -72,11 +77,12 @@ CREATE TABLE `categories` (
 INSERT INTO `categories` (`id`, `name`, `created_at`, `updated_at`) VALUES
 (1, 'Single', '2022-11-29 04:35:25', '2022-11-29 04:35:25'),
 (2, 'Double', '2022-11-29 04:35:31', '2022-11-29 04:35:31'),
-(3, 'Twin', '2022-11-29 04:35:39', '2022-11-29 04:35:39'),
 (4, 'Queen', '2022-11-29 04:35:44', '2022-11-29 04:35:44'),
 (5, 'King', '2022-11-29 04:35:49', '2022-11-29 04:35:49'),
 (6, 'Deluxe', '2022-11-29 04:35:57', '2022-11-29 04:35:57'),
-(7, 'Executive', '2022-11-29 04:36:03', '2022-11-29 04:36:03');
+(7, 'Executive', '2022-11-29 04:36:03', '2022-12-04 07:19:27'),
+(8, 'Standard', '2022-12-05 07:44:48', '2022-12-05 07:44:48'),
+(9, 'Twin', '2022-12-05 13:41:12', '2022-12-05 13:41:12');
 
 -- --------------------------------------------------------
 
@@ -101,7 +107,7 @@ CREATE TABLE `hotel-info` (
 --
 
 INSERT INTO `hotel-info` (`id`, `name`, `address`, `email`, `phone`, `logo`, `about`, `created_at`, `updated_at`) VALUES
-(1, 'La posh Hotel', 'hkjhk', 'hicode250@gmail.com', 'jhkh', 'https://res.cloudinary.com/dcwfma8py/image/upload/v1669705015/wifoo0dz5ygecr7s7qqo.jpg', 'jkhllhkj', NULL, '2022-11-29 04:56:55');
+(1, 'La posh Hotel', 'kigali, Rwanda', 'info@laposh.com', '+250780919691', 'https://res.cloudinary.com/dcwfma8py/image/upload/v1670142882/ceu73lxfqjubidlxjfju.png', 'jkhllhkj', NULL, '2022-12-05 15:21:34');
 
 -- --------------------------------------------------------
 
@@ -116,6 +122,14 @@ CREATE TABLE `messages` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `email`, `content`, `created_at`, `updated_at`) VALUES
+(1, 'hicode250@gmail.com', 'vgnjghgvhgfh', '2022-12-02 19:05:28', '2022-12-02 19:05:28'),
+(2, 'hicode250@gmail.com', 'zdfsdfsdfad', '2022-12-02 19:07:04', '2022-12-02 19:07:04');
 
 -- --------------------------------------------------------
 
@@ -182,7 +196,8 @@ INSERT INTO `model_has_roles` (`role_id`, `model_type`, `model_id`) VALUES
 (6, 'App\\Models\\User', 5),
 (6, 'App\\Models\\User', 6),
 (6, 'App\\Models\\User', 7),
-(6, 'App\\Models\\User', 8);
+(6, 'App\\Models\\User', 8),
+(6, 'App\\Models\\User', 9);
 
 -- --------------------------------------------------------
 
@@ -211,7 +226,9 @@ INSERT INTO `payments` (`id`, `booking_id`, `payment_option`, `reference`, `amou
 (4, 3, 'card', 'UHL-9596869', '64680.00', '2022-11-29 06:03:33', '2022-11-29 06:03:33'),
 (5, 4, 'card', 'MIH-4290228', '16200.00', '2022-11-29 06:06:26', '2022-11-29 06:06:26'),
 (6, 5, 'card', 'APD-6686458', '19800.00', '2022-11-29 06:07:52', '2022-11-29 06:07:52'),
-(7, 6, 'card', 'EYA-4743807', '27720.00', '2022-11-29 06:09:45', '2022-11-29 06:09:45');
+(7, 6, 'card', 'EYA-4743807', '27720.00', '2022-11-29 06:09:45', '2022-11-29 06:09:45'),
+(8, 10, 'card', 'GGL-2455938', '864.00', '2022-12-05 14:52:35', '2022-12-05 14:52:35'),
+(9, 11, 'card', 'AWZ-7467118', '432.00', '2022-12-05 16:12:23', '2022-12-05 16:12:23');
 
 -- --------------------------------------------------------
 
@@ -352,16 +369,13 @@ INSERT INTO `role_has_permissions` (`permission_id`, `role_id`) VALUES
 (19, 5),
 (20, 1),
 (21, 1),
-(21, 2),
 (21, 4),
 (22, 1),
-(22, 2),
 (22, 3),
 (23, 1),
 (23, 2),
 (23, 3),
 (24, 1),
-(24, 2),
 (24, 3);
 
 -- --------------------------------------------------------
@@ -386,11 +400,12 @@ CREATE TABLE `rooms` (
 --
 
 INSERT INTO `rooms` (`id`, `name`, `category_id`, `price`, `specifications`, `images`, `created_at`, `updated_at`) VALUES
-(1, 'King Size Four Poster', 5, '12.99', 'Our king size four poster provides views over landscaped gardens. It has a seating area, ample storage, digital safe and mini fridge.', '[\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1669703974\\/l6lake8rip5phamqprz7.jpg\"]', '2022-11-29 04:39:41', '2022-11-29 04:39:41'),
-(2, 'King Size Sleigh Bed', 5, '30.00', 'Our king size sleigh bedded also provides views over landscaped gardens. It has ample storage, a seating area, digital safe and mini fridge.', '[\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1669704027\\/pridix0gvi3et5gx9rbt.jpg\"]', '2022-11-29 04:40:31', '2022-11-29 04:40:31'),
-(3, 'Deluxe King Size', 6, '45.00', 'Our Deluxe king size room has a seating area, ample storage, digital safe and mini fridge. This room can also be configured with an extra roll-away bed for families of 3.', '[\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1669704266\\/icadbyne90bh5sbjwqqj.jpg\"]', '2022-11-29 04:44:26', '2022-11-29 04:44:26'),
-(4, 'Deluxe Twin Double', 2, '55.00', 'Our Deluxe Twin/Large Double also provides views over landscaped gardens. It has a seating area, digital safe and mini fridge. This room can be configured with either 2 single beds or zip and linked to provide a large double bed.', '[\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1669704362\\/tym2zeoj7lmbnnnehgky.jpg\"]', '2022-11-29 04:46:03', '2022-11-29 04:46:03'),
-(5, 'Compact Double', 1, '76.00', 'As our smallest budget rooms, the Compact bedrooms are suited for single occupancy or short-stay double occupancy as they have limited space and storage.', '[\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1669704398\\/fi1c1h9qwub9lqp4uwrp.jpg\"]', '2022-11-29 04:46:38', '2022-11-29 04:46:38');
+(1, 'King Size Four Poster', 1, '7.99', 'Our king size four poster provides views over landscaped gardens. It has a seating area, ample storage, digital safe and mini fridge.', '[\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1670234539\\/v9mooparcenihlekgyed.jpg\",\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1670234549\\/sadif6r53ylxv3rj9qy0.jpg\",\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1670234553\\/qyrphovkoi5uhh3sintr.jpg\"]', '2022-11-29 04:39:41', '2022-12-05 13:30:12'),
+(2, 'King Size Sleigh Bed', 4, '10.00', 'Our king size sleigh bedded also provides views over landscaped gardens. It has ample storage, a seating area, digital safe and mini fridge.', '[\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1670234471\\/gl7zeinhvosux5jymmfq.jpg\",\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1670234475\\/ipifdpuimnm7byrdo4ym.jpg\",\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1670234485\\/jbxlvaqpkek241gsufq4.jpg\"]', '2022-11-29 04:40:31', '2022-12-05 08:01:26'),
+(3, 'Deluxe King Size', 1, '5.00', 'Our Deluxe king size room has a seating area, ample storage, digital safe and mini fridge. This room can also be configured with an extra roll-away bed for families of 3.', '[\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1670234355\\/bxnlp1c8ygodst4dxkdk.jpg\",\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1670234366\\/vv99eepkkwa1pmjj4jxm.jpg\",\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1670234376\\/c8ynkeezq6pv0ihtjv8g.jpg\"]', '2022-11-29 04:44:26', '2022-12-05 07:59:37'),
+(4, 'Deluxe Twin Double', 2, '9.00', 'Our Deluxe Twin/Large Double also provides views over landscaped gardens. It has a seating area, digital safe and mini fridge. This room can be configured with either 2 single beds or zip and linked to provide a large double bed.', '[\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1670234230\\/vj7wlc9obiuu0tv9v9js.jpg\",\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1670234235\\/sd1bjt9fo2xjotdb3i5j.jpg\",\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1670234238\\/rmfwjkjzor267ayqq4o0.jpg\"]', '2022-11-29 04:46:03', '2022-12-05 07:57:20'),
+(5, 'Compact Double', 1, '5.00', 'As our smallest budget rooms, the Compact bedrooms are suited for single occupancy or short-stay double occupancy as they have limited space and storage.', '[\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1670234119\\/ypdzmeroeadxguic6dka.jpg\",\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1670234125\\/xufbvdyfkmkykq4ry9oe.jpg\",\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1670234131\\/hwhlxggaftvae5vie01x.jpg\"]', '2022-11-29 04:46:38', '2022-12-05 07:55:31'),
+(6, 'Family Room', 7, '22.00', 'Our Family room sleigh bedded also provides views over landscaped gardens. It has ample storage, a seating area, digital safe and mini fridge.', '[\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1670234762\\/fznioab3r8taby8gq9bw.jpg\",\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1670234768\\/pbpnpffyvae7cpuhtj41.jpg\",\"https:\\/\\/res.cloudinary.com\\/dcwfma8py\\/image\\/upload\\/v1670234774\\/dtbtigmfgleisqdxj3ry.jpg\"]', '2022-12-05 08:06:15', '2022-12-05 08:06:15');
 
 -- --------------------------------------------------------
 
@@ -429,6 +444,16 @@ CREATE TABLE `subscriptions` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `subscriptions`
+--
+
+INSERT INTO `subscriptions` (`id`, `email`, `created_at`, `updated_at`) VALUES
+(1, 'hicode250@gmail.com', NULL, NULL),
+(2, 'uwamahoroangel78@gmail.com', NULL, NULL),
+(3, 'jdamanibishaka@gmail.com', NULL, NULL),
+(4, 'yubahwesc@gmail.com', NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -454,14 +479,14 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `firstName`, `lastName`, `email`, `role`, `title`, `avatar`, `birthdate`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'HIRWA', 'Jean Claude', 'hicode250@gmail.com', 'Administrator', 'Mr.', 'https://res.cloudinary.com/dcwfma8py/image/upload/v1669703643/kqrqrhhmbqfbd1z9uumx.jpg', NULL, '$2y$12$MGhHPRTmLgACho4cPNeimeQG6/dpZDKdwpFdNsWTkbF9zUL.SbtFe', '2022-11-29 04:34:03', '2022-11-29 04:34:03'),
-(2, 'Angel', 'Bidas', 'uwamahoroangel78@gmail.com', 'Manager', 'Mrs.', 'https://res.cloudinary.com/dcwfma8py/image/upload/v1669704456/zu6bs4er1mpownknbdyy.jpg', NULL, '$2y$12$kwHUfw1odksHZa97aGJ7nu3iTD5uX1RocDMomB6vKfux3a3mjJwIW', '2022-11-29 04:47:38', '2022-11-29 04:47:38'),
+(1, 'HIRWA', 'Jean Claude', 'hicode250@gmail.com', 'Administrator', 'Mr.', 'https://res.cloudinary.com/dcwfma8py/image/upload/v1670236081/pwtkguxwwipuvanbhr6y.jpg', NULL, '$2y$12$MGhHPRTmLgACho4cPNeimeQG6/dpZDKdwpFdNsWTkbF9zUL.SbtFe', '2022-11-29 04:34:03', '2022-11-29 04:34:03'),
+(2, 'Angel', 'Bidas', 'uwamahoroangel78@gmail.com', 'Manager', 'Miss', 'https://res.cloudinary.com/dcwfma8py/image/upload/v1670236061/mghffiywo4569v1ff7lm.jpg', NULL, '$2y$12$kwHUfw1odksHZa97aGJ7nu3iTD5uX1RocDMomB6vKfux3a3mjJwIW', '2022-11-29 04:47:38', '2022-11-29 04:47:38'),
 (3, 'AHIMANA', 'Jado', 'intiti@gmail.com', 'Accountant', 'Mr.', 'https://res.cloudinary.com/dcwfma8py/image/upload/v1669704521/gqyst7gdoidacncx8cyd.jpg', NULL, '$2y$12$HPg9HHXRIZWx8reERSQpLuVANICCtLyw65clPbdl6c6Fp.36oL.3G', '2022-11-29 04:48:43', '2022-11-29 04:48:43'),
 (4, 'MUNEZERO', 'Nadine', 'nadine@gmail.com', 'Room-operator', 'Miss.', 'https://res.cloudinary.com/dcwfma8py/image/upload/v1669704593/wakovajrtuqedpehbyqy.jpg', NULL, '$2y$12$zl0m./RVjL0QiK41IWew4u/5yZxEYdPAxF/rUST5pD9pcuZPddrtW', '2022-11-29 04:49:54', '2022-11-29 04:49:54'),
-(5, 'HABWIKUZO', 'Henriette', 'hicode251@gmail.com', 'client', 'Miss.', NULL, '2001-07-16', '$2y$12$tpBZdAAKox6jOc3jHTd3SO0uNauYwb4MDIDXYpVhBS15yj8EqSqfu', '2022-11-29 04:51:11', '2022-11-29 04:51:11'),
+(5, 'HABWIKUZO', 'Henriette', 'hicode251@gmail.com', 'client', 'Miss.', 'https://dl.memuplay.com/new_market/img/com.vicman.newprofilepic.icon.2022-06-07-21-33-07.png', '2001-07-16', '$2y$12$tpBZdAAKox6jOc3jHTd3SO0uNauYwb4MDIDXYpVhBS15yj8EqSqfu', '2022-11-29 04:51:11', '2022-11-29 04:51:11'),
 (6, 'KARASISI', 'Trophime', 'hicode252@gmail.com', 'client', 'Mr.', NULL, '2001-07-27', '$2y$12$fX3AQzmJn5Yo7Yn0TgWL9eYd7cOwIx89fAYWt2kx522zoXlsPE8uG', '2022-11-29 04:51:54', '2022-11-29 04:51:54'),
 (7, 'CYIZERE', 'Nadege', 'hicode253@gmail.com', 'client', 'Miss.', NULL, '2003-08-28', '$2y$12$ZFbl86txkxa5Fj8so/r.kuAH6byi2PrSGftbybXJgjaZt6oAIdbwK', '2022-11-29 04:52:36', '2022-11-29 04:52:36'),
-(8, '75', 'Jean Claude', 'wrewwer@gmail.com', 'client', 'Mr.', NULL, NULL, '$2y$12$RCIw6t1XDf95uyvXv8j7YOef3UruexBhCWcLjBIxfspQBkIimjcHa', '2022-11-29 06:36:44', '2022-11-29 06:36:44');
+(9, 'HIRWA', 'Jean Claude', 'hicode25@gmail.com', 'client', 'Mr.', NULL, NULL, '$2y$12$GIg/p99SHxl0kPQRvZiQ3evN0muxVFx6dccY1u3vJl2hgnCCdYmIq', '2022-12-05 18:22:25', '2022-12-05 18:22:25');
 
 --
 -- Indexes for dumped tables
@@ -585,13 +610,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `hotel-info`
@@ -603,7 +628,7 @@ ALTER TABLE `hotel-info`
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `migrations`
@@ -615,7 +640,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `payments`
 --
 ALTER TABLE `payments`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `permissions`
@@ -639,7 +664,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT for table `rooms`
 --
 ALTER TABLE `rooms`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `status`
@@ -651,13 +676,13 @@ ALTER TABLE `status`
 -- AUTO_INCREMENT for table `subscriptions`
 --
 ALTER TABLE `subscriptions`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
